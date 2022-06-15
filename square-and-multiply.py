249ebac93c5d0e18ff20062cfbab2ff40149a88a
Joel L <joelius300@protonmail.com>
2022-06-15 17:17:07 +0200

    god complex

--- square-and-multiply.py
+++ square-and-multiply.py
@@ -1,8 +1,13 @@
-from dmath import square_and_multiply
+import sys
+
+if sys.platform == "nspire":
+    with open("/documents/apps/dmath.py.tns", "r") as file:
+        exec(file.read())
+else:
+    from dmath import square_and_multiply
 
 base = int(input('Basis: '))
 expo = int(input('Exponent: '))
 mod = int(input('Modulo: '))
 
 square_and_multiply(base, expo, mod)
-
