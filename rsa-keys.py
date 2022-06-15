2a75befc077e3d0f2a1f48642b4328495100078c
Joel L <joelius300@protonmail.com>
2022-06-15 17:12:26 +0200

    running it with exec works! now figure out how to distinguish between development and ti

--- rsa-keys.py
+++ rsa-keys.py
@@ -1,4 +1,9 @@
-# from dmath import generate_rsa_keys apparently imports don't work
+import sys
+print(sys.version)
+print(sys.platform)
+print(sys.version_info)
+
+# from dmath import generate_rsa_keys
 
 with open("/documents/apps/dmath.py.tns", "r") as file:
     exec(file.read())  # define functions?
@@ -9,4 +14,4 @@ e = int(input('e (teilerfremd zu phi(pq)={}): '.format((p - 1) * (q - 1))))
 
 (n, e), d = generate_rsa_keys(p, q, e)
 
-print('Public key: ({}, {})  |  Private key: {}'.format(n, e, d))
+print('Public key: (n={}, e={})  |  Private key: d={}'.format(n, e, d))
